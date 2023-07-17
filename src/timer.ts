172b8b3c2c54cf5bfb05c99e7d73229cd3d8f56d
Michal Grňo <m93a.cz@gmail.com>
2023-07-17 15:57:27 +0200

    working on config stores

--- src/timer.ts
+++ src/timer.ts
@@ -1,23 +1,55 @@
+interface Readable<T> {
+    get(): T;
+}
+const isReadable = <T>(x: unknown): x is Readable<T> =>
+    typeof x === "object" &&
+    x !== null &&
+    "get" in x &&
+    typeof x.get === "function";
+const get = <T>(x: T | Readable<T>) => (isReadable(x) ? x.get() : x);
+interface Disposable {
+    dispose(): void;
+}
+const isDisposable = (x: unknown): x is Disposable =>
+    typeof x === "object" &&
+    x !== null &&
+    "dispose" in x &&
+    typeof x.dispose === "function";
+const dispose = (x: unknown): void => {
+    if (isDisposable(x)) x.dispose();
+};
+
 /** ![](https://upload.wikimedia.org/wikipedia/commons/thumb/4/45/Kitchen_timer.jpg/120px-Kitchen_timer.jpg) */
 export class Timer {
-    private interval: number;
+    private interval: number | Readable<number>;
+    private lastIntervalValue: number;
+
     private readonly repeating: boolean;
     private callback: () => void;
 
     private timerId: number = -1;
     public ticking = false;
 
-    constructor({ interval, repeating, callback }: { interval: number, callback: () => void, repeating?: boolean }) {
+    constructor({
+        interval,
+        repeating,
+        callback,
+    }: {
+        interval: number;
+        callback: () => void;
+        repeating?: boolean;
+    }) {
         this.interval = interval;
+        this.lastIntervalValue = get(this.interval);
         this.callback = callback;
         this.repeating = repeating ?? false;
     }
 
     private setTimer(f: () => void): number {
         if (this.repeating) {
-            return +setInterval(f, this.interval);
+            return +setInterval(f, get(this.interval));
         } else {
-            return +setTimeout(f, this.interval);
+            return +setTimeout(f, get(this.interval));
         }
     }
 
@@ -34,7 +66,15 @@ export class Timer {
 
         this.clearTimer(this.timerId);
         this.timerId = this.setTimer(() => {
-            this.ticking = false;
+            if (this.repeating) {
+                const currentIntervalValue = get(this.interval);
+                if (currentIntervalValue !== this.lastIntervalValue) {
+                    this.start();
+                }
+            } else {
+                this.ticking = false;
+            }
+
             this.callback();
         });
     }
@@ -47,6 +87,9 @@ export class Timer {
     dispose() {
         this.stop();
         this.callback = () => {};
-        this.start = () => { throw new Error("Calling a timer that has been disposed of."); };
+        this.start = () => {
+            throw new Error("Calling a timer that has been disposed of.");
+        };
+        dispose(this.interval);
     }
 }
